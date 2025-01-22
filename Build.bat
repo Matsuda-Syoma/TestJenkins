15aeb0a5c65b95e130b8a2f6d28806559d0a7764
Matsuda-Syoma <122336837+Matsuda-Syoma@users.noreply.github.com>
2025-01-22 10:56:01 +0900

    batファイル追加

---
+++ Build.bat
@@ -0,0 +1,4 @@
+@echo off
+"C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MsBuild.exe" TestJenkins\testJenkins.sln /t:rebuild
+cd TestJenkins\x64\Debug
+TestJenkins.exe
--- TestJenkins/main.cpp
+++ TestJenkins/main.cpp
@@ -6,5 +6,7 @@ int main()
 	int num1 = 2;
 	int num2 = 3;
 
+	printf("%d + %d = %d‚Å‚·B\n", num1, num2, num1 + num2);
+
 	return 0;
 }
