2272846d51ec6b0eafa23ce35525106a65fddd51
Matsuda-Syoma <122336837+Matsuda-Syoma@users.noreply.github.com>
2025-01-22 11:08:48 +0900

    bat更新

--- Build.bat
+++ Build.bat
@@ -2,5 +2,4 @@
 chcp 65001
 "C:\Program Files\Microsoft Visual Studio\2022\Community\Msbuild\Current\Bin\MsBuild.exe" TestJenkins\testJenkins.sln /t:rebuild
 cd TestJenkins\x64\Debug
-chcp 65001
 TestJenkins.exe
